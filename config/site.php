9871e78a42a7f37f7562e6bc2c7c89ce2e939641
VishalKumar6767 <kumarvishal27697@gmail.com>
2024-06-19 12:13:29 +0530

    adding message on goroup list type if there is no expenses and changging url  user for invitationgroup

--- config/site.php
+++ config/site.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-  
-    'frontWebsiteUrl' => 'http://192.168.1.64:5173/signup', // Use string format for URL
+
+    'frontWebsiteUrl' => 'https://splitmoney.tech/signup', // Use string format for URL
 ];
